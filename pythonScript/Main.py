508d6326b6aa68500edfe222376179d1db8e900a
Nokkky <38582240+Nokkky@users.noreply.github.com>
2020-10-26 13:01:53 -0700

    Add files via upload
    Script used to create database schema and tables' structure.

---
+++ pythonScript/Main.py
@@ -0,0 +1,32 @@
+import json
+from Table import Table
+from create_database import connect_to_mysql, switch_to_database, create_tables
+
+# read cRonfig file
+with open('config.txt') as f:
+    file = json.load(f)
+
+# config: dictionary stores parameters to connect to mySQL server
+config = {}
+config['user'] = file['user']
+config['password'] = file['password']
+config['host'] = file['host']
+
+# DB_NAME: name of new database schema
+DB_NAME = file['database'] + str(1)
+
+# connect to mysql
+# cnx: MySQL connection object
+cnx = connect_to_mysql(config)
+
+# switch or create database schema
+switch_to_database(cnx, DB_NAME)
+
+# create 36 tables under the database
+# TABLES: predefined CREATE SQL commands
+TABLES = Table.TABLES
+create_tables(TABLES, cnx)
+
+cnx.close()
+
+
---
+++ pythonScript/ReadMe.md
@@ -0,0 +1,8 @@
+## To run scripts:
+1. Enter MySQL connect parameter at confit.txt file
+2. Run Main.py script in terminal under correct python version
+
+## Package version
+1. python 3.7.3
+2. mysql-connector-python 8.0.21
+    * [Guide to install mysql-connector-python](https://dev.mysql.com/doc/connector-python/en/connector-python-installation.html)
